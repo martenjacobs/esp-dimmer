d7ed514989c63b0ce17797a5214ea7de4513f0ca
Marten Jacobs <mail@vanmarten.nl>
2018-05-14 20:45:36 +0200

    Added some comments to config

--- src/config.hpp
+++ src/config.hpp
@@ -3,22 +3,27 @@
 
 #include <constants.hpp>
 
+// WiFi connection information
 #define wifi_ssid "<SSID>"
 #define wifi_password "<KEY>"
 #define wifi_wait_sec 10
 
+// Information used to start up an AP when connecting to WiFi fails,
+// so you can push firmware through OTA
 #define ap_ssid "esp-dimmer-1"
 #define ap_psk ""
 #define ap_ip IPAddress(192, 168, 43, 1)
 #define ap_gw IPAddress(192, 168, 43, 1)
 #define ap_sn IPAddress(255, 255, 255, 0)
 
+// The connection information for the MQTT server
 #define mqtt_server "<HOST>"
 #define mqtt_port <PORT>
 #define mqtt_user "<MQTT_USER>"
 #define mqtt_password "<MQTT_PASSWORD>"
 #define mqtt_client_id "<UNIQUE_CLIENT_ID>"
 
+// You probably don't need to change anything here, but you can
 #define mqtt_namespace mqtt_client_id "/"
 #define mqtt_pub_topic_ip mqtt_namespace "ip-address"
 #define mqtt_pub_topic_log mqtt_namespace "log"
@@ -41,29 +46,35 @@
 #define mqtt_sub_topic_eeprom_read mqtt_namespace "eeprom/read"
 #define mqtt_sub_topic_eeprom_write mqtt_namespace "eeprom/write"
 
+// The interval at which the module publishes a full state report to MQTT
 #define INFO_PUBLISH_INTERVAL 300000
 
+// Enables updating the firmware over WiFi (you probably want this)
 #define ENABLE_OTA 1
+
+// Enables the device to connect to an MQTT broker
 #define ENABLE_MQTT 1
 
+// Enables the gates
 #define ENABLE_GATE_1 1
 #define ENABLE_GATE_2 1
 
+// Enable dimming (otherwise the device is a simple on/off switch)
 #define ENABLE_DIMMER 1
 
+// Enable channel locking. Channel locking is used for blinds, and makes sure
+// the channels can't be switched on simultaneously
 #define CHANNEL_LOCK 0
 
+// Writes every state change to EEPROM so it persists across restarts. If
+// disabled, the start-up state is set by publishing a message to the
+// mqtt_sub_topic_eeprom_write topic
 #define PERSIST_STATE 1
 
+// Set up how the GPIOs are wired up
 #define SETUP_GPIO4_INPUT CONTROL_TOGGLE_BUTTON
 #define SETUP_GPIO4_CONTROL GATE_1
 #define SETUP_GPIO5_INPUT CONTROL_TOGGLE_BUTTON
 #define SETUP_GPIO5_CONTROL GATE_2
-
-//#define SETUP_GPI12_INPUT CONTROL_PULSE_BUTTON
-//#define SETUP_GPI12_CONTROL GATE_BOTH
-//#define SETUP_GPI14_INPUT CONTROL_PULSE_BUTTON
-//#define SETUP_GPI14_CONTROL GATE_BOTH
-
 
 #endif
